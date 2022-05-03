64b863113588d27b3694f5dc18a5f2e44098c037
NelsonGamero <nelson.gamero96@gmail.com>
2022-05-03 19:58:31 -0300

    Se cambia HashRouter por BrowserRouter para crear servidor con Node js

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import "./main.scss";
-import App from "./App";
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import './main.scss';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-const app = document.getElementById("root");
+const app = document.getElementById('root');
 
 createRoot(app).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
